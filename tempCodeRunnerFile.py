2a46ffa5e8b1a5466c723669d94bb74b9a350b96
Sakib Noman <skbnmn@gmail.com>
2021-02-12 22:47:56 +0600

    string

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+a = int(input())
+    # b = int(input())
+    # hi = a+b
---
+++ test.py
@@ -0,0 +1,24 @@
+
+#Started learning python
+
+# x =2
+# y =3
+# if x>y:
+#     print("x is bigger")
+# else:
+#     print("y is bigger")
+
+# t = int(input())
+# case = 0
+# for i in range(1,t+1):
+#     case = case + 1
+#     a = int(input())
+#     b = int(input())
+#     add = a+b
+#     print("Case",str(case)+":",add)
+
+# x = 6
+# x6 = complex(x)
+# print(x6)
+import random
+print(random.randrange(1,10))
